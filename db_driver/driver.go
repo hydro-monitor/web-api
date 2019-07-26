ee48fa2e4dd5754622ae98c9740acfe09e6537e7
Manuel Porto <manu.porto94@gmail.com>
2019-07-26 13:28:53 -0300

    Make database driver a singleton

--- db_driver/driver.go
+++ db_driver/driver.go
@@ -1,24 +1,35 @@
 package db_driver
 
-import "github.com/gocql/gocql"
+import (
+	"github.com/gocql/gocql"
+	"log"
+)
 
 type Driver struct {
 	cluster *gocql.ClusterConfig
 	session *gocql.Session
 }
 
-func NewDriver() *Driver {
+var driver *Driver
+
+func init() {
+	driver = newDriver()
+}
+
+func newDriver() *Driver {
 	// TODO get this info from an env file or similar
 	cluster := gocql.NewCluster("192.168.50.41", "192.168.50.42", "192.168.50.43")
 	cluster.Keyspace = "usertest"
 	cluster.PoolConfig.HostSelectionPolicy = gocql.RoundRobinHostPolicy()
-	return &Driver{cluster: cluster}
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+	return &Driver{cluster: cluster, session: session}
 }
 
-func (d *Driver) StartSession() error {
-	session, err := d.cluster.CreateSession()
-	d.session = session
-	return err
+func GetDriver() *Driver {
+	return driver
 }
 
 func (d *Driver) EndSession() {
