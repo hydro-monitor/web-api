40b1317961d470c8baa49ff4860ef663d34c4df0
Manuel Porto <manu.porto94@gmail.com>
2019-09-30 17:57:26 -0300

    Read database configuration from .env file

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/gocql/gocql v0.0.0-20190717234527-2ba2dd7440dc
 	github.com/google/pprof v0.0.0-20190723021845-34ac40c74b70 // indirect
 	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.6
 	github.com/scylladb/gocqlx v1.3.1
 	golang.org/x/arch v0.0.0-20190312162104-788fe5ffcd8c // indirect
--- pkg/db_driver/driver.go
+++ pkg/db_driver/driver.go
@@ -2,7 +2,10 @@ package db_driver
 
 import (
 	"github.com/gocql/gocql"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
+	"strings"
 )
 
 type Driver struct {
@@ -13,13 +16,18 @@ type Driver struct {
 var driver *Driver
 
 func init() {
+	// TODO do this in the program's entry point
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	driver = newDriver()
 }
 
 func newDriver() *Driver {
-	// TODO get this info from an env file or similar
-	cluster := gocql.NewCluster("192.168.50.41", "192.168.50.42", "192.168.50.43")
-	cluster.Keyspace = "hydromonitor"
+	cluster := gocql.NewCluster()
+	cluster.Hosts = strings.Split(os.Getenv("DB_HOSTS"), ",")
+	cluster.Keyspace = os.Getenv("DB_KEYSPACE")
 	cluster.PoolConfig.HostSelectionPolicy = gocql.RoundRobinHostPolicy()
 	session, err := cluster.CreateSession()
 	if err != nil {
