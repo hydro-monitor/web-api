1d66e7b39895a14157000c3b06def4f14f45d6f1
Manuel Porto <manu.porto94@gmail.com>
2020-01-14 09:08:11 -0300

    Refactor repository interface and implementation

--- pkg/repositories/node_repository.go
+++ pkg/repositories/node_repository.go
@@ -5,25 +5,12 @@ import (
 	"hydro_monitor/web_api/pkg/clients/db"
 )
 
-type nodeRepositoryImpl struct {
-	table    *table.Table
-	dbClient db_client.DbClient
-}
-
-func NewNodeRepository(dbClient db_client.DbClient) Repository {
+func NewNodeRepository(client db.Client) Repository {
 	nodeMetadata := table.Metadata{
 		Name:    "nodes",
 		Columns: []string{"id", "description", "configuration", "state", "manual_reading"},
 		PartKey: []string{"id"},
 		SortKey: nil,
 	}
-	return &nodeRepositoryImpl{table: table.New(nodeMetadata), dbClient: dbClient}
-}
-
-func (n *nodeRepositoryImpl) Get(args interface{}) error {
-	return n.dbClient.Get(n.table, args)
-}
-
-func (n *nodeRepositoryImpl) Insert(args interface{}) error {
-	panic("implement me")
+	return &repositoryImpl{table: table.New(nodeMetadata), client: client}
 }
--- pkg/repositories/repository.go
+++ pkg/repositories/repository.go
@@ -1,6 +1,24 @@
 package repositories
 
+import (
+	"github.com/scylladb/gocqlx/table"
+	"hydro_monitor/web_api/pkg/clients/db"
+)
+
 type Repository interface {
 	Get(args interface{}) error
 	Insert(args interface{}) error
+}
+
+type repositoryImpl struct {
+	table  *table.Table
+	client db.Client
+}
+
+func (r repositoryImpl) Get(args interface{}) error {
+	return r.client.Get(r.table, args)
+}
+
+func (r repositoryImpl) Insert(args interface{}) error {
+	panic("implement me")
 }
