251e3531879dae2e63120952f3106fc23009bb72
Manuel Porto <manu.porto94@gmail.com>
2020-02-09 18:35:04 -0300

    Delete more old code

--- pkg/models/node.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Node struct {
-	Id            string `json:"id"`
-	Description   string `json:"description"`
-	Configuration string `json:"configuration"`
-	State         string `json:"state"`
-	ManualReading bool   `json:"manual_reading"`
-}
--- pkg/models/node_configuration.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "hydro_monitor/web_api/pkg/models/api_models"
-
-type NodeConfiguration struct {
-	NodeId string                       `json:"nodeId"`
-	States map[string]*api_models.State `json:"states"`
-}
