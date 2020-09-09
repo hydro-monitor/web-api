d4314c8a5c78d824dc0b72a3394f1c5f99042a50
Manuel Porto <manuel.porto@mercadolibre.com>
2020-09-08 21:53:47 -0300

    Log auth header

--- pkg/middlewares/jwt_config.go
+++ pkg/middlewares/jwt_config.go
@@ -7,6 +7,7 @@ import (
 
 func JWTSkipper(allowedRequests []Request) middleware.Skipper {
 	return func(c echo.Context) bool {
+		c.Logger().Errorf("Authorization: %s", c.Request().Header.Get("Authorization"))
 		request := Request{
 			Method: c.Request().Method,
 			Path:   c.Path(),
